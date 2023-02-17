df4a78d452136998fc04619384baa63027c65a26
Kaiser <cesar.paganvillafane@gmail.com>
2023-02-17 12:08:47 +0100

    Tarea1 - Fin
    Co-Authored-By: angeltox <67331777+angeltox@users.noreply.github.com>

--- proyecto/src/main/java/arso/proyecto/Tarea1.java
+++ proyecto/src/main/java/arso/proyecto/Tarea1.java
@@ -9,14 +9,10 @@ import java.util.Scanner;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
 
 public class Tarea1 {
 
@@ -33,12 +29,14 @@ public class Tarea1 {
 		// 2. Pedir a la factoría la construcción del analizador
 		DocumentBuilder analizador = factoria.newDocumentBuilder();
 		// 3. Analizar el documento
-		String API = "http://api.geonames.org/findNearbyPostalCodes?";
+		String API = "http://api.geonames.org/findNearbyWikipedia?";
 		String parameters = "postalcode=" + codigoPostal + "&country=" + codigoPais + "&radius=10&username=arso";
 		URL url = new URL(API + parameters);
 		downloadFile(url, "xml/codigopostal.xml");
 		Document documento = analizador.parse("xml/codigopostal.xml");
-
+		
+		consultarTitulos(documento);
+		
 //		// 1. Construye la factoría de transformación y obtiene un transformador
 //		TransformerFactory tFactoria = TransformerFactory.newInstance();
 //		Transformer transformacion = tFactoria.newTransformer();
@@ -49,7 +47,8 @@ public class Tarea1 {
 //		Result output = new StreamResult(fichero);
 //		// 4. Aplica la transformación
 //		transformacion.transform(input, output);
-	}
+
+		}
 
 	public static void downloadFile(URL url, String fileName) throws Exception {
 		File archivo = new File(fileName);
@@ -59,6 +58,17 @@ public class Tarea1 {
 
 		try (InputStream in = url.openStream()) {
 			Files.copy(in, Paths.get(fileName));
+		}
+	}
+	
+	public static void consultarTitulos(Document documento) {
+		NodeList elementos = documento.getElementsByTagName("entry");
+		for (int i = 0; i < elementos.getLength() ; i++) {
+			Element elemento = (Element) elementos.item(i);
+			NodeList titulos = elemento.getElementsByTagName("title");
+			Element titulo = (Element) titulos.item(0);
+			String tituloString = titulo.getTextContent();
+			System.out.println(tituloString);
 		}
 	}
 

3028e703f03cbd8094cc61ef1cdad082e254a248
Kaiser <cesar.paganvillafane@gmail.com>
2023-02-17 10:35:55 +0100

    Commit Inicial
    Co-Authored-By: angeltox <67331777+angeltox@users.noreply.github.com>

---
+++ proyecto/src/main/java/arso/proyecto/App.java
@@ -0,0 +1,13 @@
+package arso.proyecto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
